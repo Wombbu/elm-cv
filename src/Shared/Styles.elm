b6f0000146832b8f5b3532132e4a34efacf5da3d
Lauri Nevanperä <lauri.nevanpera@sasken.com>
2017-01-24 20:33:30 +0200

    Add color styles

--- src/Shared/Styles.elm
+++ src/Shared/Styles.elm
@@ -9,13 +9,38 @@ import Html.Attributes
 -- Colors
 
 
-colorMain: Color
+colorMain : Color
 colorMain =
   hex "#333c37"
 
-colorHighlight: Color
+colorHighlight : Color
 colorHighlight =
   hex "#fff"
+
+colorTextArea : Color
+colorTextArea =
+  rgb 30 33 39
+
+colorSidebarBg : Color
+colorSidebarBg =
+  rgb 25 28 32
+
+colorSidebarHilight : Color
+colorSidebarHilight =
+  rgb 33 36 44
+
+colorTextMain : Color
+colorTextMain =
+  rgb 139 147 164
+
+colorTextHilight : Color
+colorTextHilight =
+  rgb 204 208 216
+
+colorBlue : Color
+colorBlue =
+  rgb 94 138 255
+
 
 
 -- Translate mixins to native elm styles
