d8f704f994826aa163c3947e1220750dbace4ac2
Lauri Nevanperä <lauri.nevanpera@sasken.com>
2017-02-15 18:35:38 +0200

    Add support for fetching&parsing json data, minor tweaks to json structure

---
+++ src/App/Http.elm
@@ -0,0 +1,54 @@
+module App.Http exposing (getCvData)
+
+import Http
+import Json.Decode as Decode exposing (int, string, field, list)
+import App.Types as Types
+
+
+getCvData : Http.Request Types.CvData
+getCvData =
+    Http.get "./combined.json" decodeCvData
+
+
+decodeCvData : Decode.Decoder Types.CvData
+decodeCvData =
+    Decode.map5 Types.CvData
+        (field "info" decodeInfo)
+        (field "projects" (list decodeProjects))
+        (field "employers" (list decodeEmployers))
+        (field "languages" (list decodeLanguages))
+        (field "tools" (list string))
+
+
+decodeInfo : Decode.Decoder Types.Info
+decodeInfo =
+    Decode.map6 Types.Info
+        (field "name" string)
+        (field "born" int)
+        (field "interests" (list string))
+        (field "location" string)
+        (field "photo" string)
+        (field "education" string)
+
+
+decodeProjects : Decode.Decoder Types.Project
+decodeProjects =
+    Decode.map3 Types.Project
+        (field "name" string)
+        (field "video" string)
+        (field "explanation" string)
+
+
+decodeEmployers : Decode.Decoder Types.Employer
+decodeEmployers =
+    Decode.map2 Types.Employer
+        (field "name" string)
+        (field "description" (list string))
+
+
+decodeLanguages : Decode.Decoder Types.Language
+decodeLanguages =
+    Decode.map3 Types.Language
+        (field "language" string)
+        (field "technologies" (list string))
+        (field "skill" int)
--- src/App/Types.elm
+++ src/App/Types.elm
@@ -17,5 +17,46 @@ type alias Model =
     { sideBarFolders : List SideBar.Types.Model
     , tabs : List TabBar.Types.Model
     , textArea : TextArea.Types.Model
-    , renderFunction : TextArea.Types.Model -> Html TextArea.Types.Msg
+    , renderFunction :
+        TextArea.Types.Model -> Html TextArea.Types.Msg
+        -- , cvData : CvData
+    }
+
+
+type alias CvData =
+    { info : Info
+    , projects : List Project
+    , employers : List Employer
+    , languages : List Language
+    , tools : List String
+    }
+
+
+type alias Info =
+    { name : String
+    , born : Int
+    , interests : List String
+    , location : String
+    , photo : String
+    , education : String
+    }
+
+
+type alias Project =
+    { name : String
+    , video : String
+    , explanation : String
+    }
+
+
+type alias Employer =
+    { name : String
+    , description : List String
+    }
+
+
+type alias Language =
+    { language : String
+    , technologies : List String
+    , skill : Int
     }
